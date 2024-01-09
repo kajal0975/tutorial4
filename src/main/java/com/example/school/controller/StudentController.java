f58ce6f4b06da224fe83f7bb05521fc0449d9b66
kajal0975 <kajalksingh0975@gmail.com>
2024-01-09 12:13:41 +0000

    solution

---
+++ src/main/java/com/example/school/controller/StudentController.java
@@ -0,0 +1,47 @@
+package com.example.school.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.school.model.Student;
+import com.example.school.service.StudentH2Service;
+
+import java.util.ArrayList;
+
+@RestController
+public class StudentController {
+    @Autowired
+    private StudentH2Service studentH2Service;
+
+    @GetMapping("/students")
+    public ArrayList<Student> getStudents() {
+        return studentH2Service.getStudents();
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudentById(@PathVariable("studentId") int studentId) {
+        return studentH2Service.getStudentById(studentId);
+    }
+
+    @PostMapping("/students")
+    public Student addStudent(@RequestBody Student student) {
+        return studentH2Service.addStudent(student);
+    }
+
+    @PostMapping("/students/bluk")
+    public String addMultipleStudents(@RequestBody ArrayList<Student> studentsList) {
+        return studentH2Service.addMultipleStudents(studentsList);
+    }
+
+    @PutMapping("/students/{studentId}")
+    public Student updateStudent(@PathVariable("studentId") int studentId, @RequestBody Student student) {
+        return studentH2Service.updatStudent(studentId, student);
+    }
+
+    @DeleteMapping("/students/{studentId}")
+    public void deleteStudent(@PathVariable int studentId) {
+        studentH2Service.deleteStudent(studentId);
+    }
+}
+
+
---
+++ src/main/java/com/example/school/model/StudentRowMapper.java
@@ -0,0 +1,16 @@
+package com.example.school.model;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class StudentRowMapper implements RowMapper<Student> {
+    public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Student(
+                rs.getInt("StudentId"),
+                rs.getString("StudentName"),
+                rs.getString("Gender"),
+                rs.getInt("Standard"));
+    }
+}
